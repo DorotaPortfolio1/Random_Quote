3e7d1944d7a317279befd7e7609922ad49fbb104
Dorota Kazak <dorota.kazak@gmail.com>
2018-05-28 19:28:38 +0200

    Fix scripts

--- js/scripts.js
+++ js/scripts.js
@@ -16,12 +16,12 @@ function createTweet(input) {
     var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
     if (tweetText.length > 140) {
     getQuote();
-} else {
+    } else {
     var tweet = tweetLink + encodeURIComponent(tweetText);
     $('.quote').text(quoteText);
     $('.author').text("Author: " + quoteAuthor);
+    $('.tweet').attr('href', tweet);
 }
-$('.tweet').attr('href', tweet);
 }
 $(document).ready(function() {
     getQuote();
